409c7d751f4a7d02de52c5a8f42170cd96125fb0
resucito <brandagamba@tutanota.com>
2020-06-27 16:32:54 -0500

    Rename Resucito.Blazor => Dictionary

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Resucito.Blazor
+namespace Dictionary
 {
     public class Program
     {
