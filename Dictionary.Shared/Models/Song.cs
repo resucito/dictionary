f15bbfc9e2149fd7cc14d2b428ec0f5772f4e400
resucito <brandagamba@tutanota.com>
2020-07-11 16:56:36 -0500

    navigate to search results from home

--- Dictionary.Shared/Models/Song.cs
+++ Dictionary.Shared/Models/Song.cs
@@ -4,13 +4,13 @@ using System.Text.Json.Serialization;
 namespace Dictionary.Shared.Models
 {
     public class Song
-    {
+    {        
         [JsonPropertyName("original_title")]
         public string OriginalTitle { get; set; }
         [JsonPropertyName("spanish_title")]
         public string SpanishTitle { get; set; }
         [JsonPropertyName("english_title")]
-        public string EnglishTitle { get; set; }
+        public string EnglishTitle { get; set; }        
 
         public bool IsMatch(string query)
         {
